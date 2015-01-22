1d2d0dfe4860f6b0101e253b2098a61fa438678f
Debat Johan <johan.debat@gmail.com>
2015-01-22 12:25:07 +0100

    Game class

--- app/src/main/java/com/example/jdebat/andromind/classes/Game.java
+++ app/src/main/java/com/example/jdebat/andromind/classes/Game.java
@@ -3,11 +3,19 @@ package com.example.jdebat.andromind.classes;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.ArrayList;
+
 /**
  * Created by jdebat on 22/01/15.
  */
 public class Game implements Parcelable {
 
+    private String serverPlayer;
+    private ArrayList<String> clientsPlayer;
+
+    private int nbJoueurs;
+
+    private String codeGagnant[] = new String[4];
 
 
     @Override
